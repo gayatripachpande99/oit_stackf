fc027fac458eefa90e3b4dcb3ac7fbdb4dd9a36f
gayatripachpande99 <gayatripachpande99@users.noreply.github.com>
2026-04-08 19:40:27 +0530

    Implemented dynamic district constituency pages entirely from a scalable script and added Instagram links across all website footers

---
+++ constituencies.js
@@ -0,0 +1,470 @@
+const districtData = {
+    "nandurbar": {
+        name: "Nandurbar",
+        assemblies: [
+            "1 - Akkalkuwa (ST)",
+            "2 - Shahada (ST)",
+            "3 - Nandurbar (ST)",
+            "4 - Nawapur (ST)"
+        ]
+    },
+    "dhule": {
+        name: "Dhule",
+        assemblies: [
+            "5 - Sakri (ST)",
+            "6 - Dhule Rural",
+            "7 - Dhule City",
+            "8 - Sindhkheda",
+            "9 - Shirpur (ST)"
+        ]
+    },
+    "jalgaon": {
+        name: "Jalgaon",
+        assemblies: [
+            "10 - Chopda (ST)",
+            "11 - Raver",
+            "12 - Bhusawal (SC)",
+            "13 - Jalgaon City",
+            "14 - Jalgaon Rural",
+            "15 - Amalner",
+            "16 - Erandol",
+            "17 - Chalisgaon",
+            "18 - Pachora",
+            "19 - Jamner",
+            "20 - Muktainagar"
+        ]
+    },
+    "buldhana": {
+        name: "Buldhana",
+        assemblies: [
+            "21 - Malkapur",
+            "22 - Buldhana",
+            "23 - Chikhli",
+            "24 - Sindhkhed Raja",
+            "25 - Mehkar (SC)",
+            "26 - Khamgaon",
+            "27 - Jalgaon (Jamod)"
+        ]
+    },
+    "akola": {
+        name: "Akola",
+        assemblies: [
+            "28 - Akot",
+            "29 - Balapur",
+            "30 - Akola West",
+            "31 - Akola East",
+            "32 - Murtizapur (SC)"
+        ]
+    },
+    "washim": {
+        name: "Washim",
+        assemblies: [
+            "33 - Risod",
+            "34 - Washim (SC)",
+            "35 - Karanja"
+        ]
+    },
+    "amravati": {
+        name: "Amravati",
+        assemblies: [
+            "36 - Dhamangaon Railway",
+            "37 - Badnera",
+            "38 - Amrawati",
+            "39 - Teosa",
+            "40 - Daryapur (SC)",
+            "41 - Melghat (ST)",
+            "42 - Achalpur",
+            "43 - Morshi"
+        ]
+    },
+    "wardha": {
+        name: "Wardha",
+        assemblies: [
+            "44 - Arvi",
+            "45 - Deoli",
+            "46 - Hinganghat",
+            "47 - Wardha"
+        ]
+    },
+    "nagpur": {
+        name: "Nagpur",
+        assemblies: [
+            "48 - Katol",
+            "49 - Savner",
+            "50 - Hingna",
+            "51 - Umred (SC)",
+            "52 - Nagpur South West",
+            "53 - Nagpur South",
+            "54 - Nagpur East",
+            "55 - Nagpur Central",
+            "56 - Nagpur West",
+            "57 - Nagpur North (SC)",
+            "58 - Kamthi",
+            "59 - Ramtek"
+        ]
+    },
+    "bhandara": {
+        name: "Bhandara",
+        assemblies: [
+            "60 - Tumsar",
+            "61 - Bhandara (SC)",
+            "62 - Sakoli"
+        ]
+    },
+    "gondia": {
+        name: "Gondia",
+        assemblies: [
+            "63 - Arjuni Morgaon (SC)",
+            "64 - Tirora",
+            "65 - Gondia",
+            "66 - Amgaon (ST)"
+        ]
+    },
+    "gadchiroli": {
+        name: "Gadchiroli",
+        assemblies: [
+            "67 - Armori (ST)",
+            "68 - Gadchiroli (ST)",
+            "69 - Aheri (ST)"
+        ]
+    },
+    "chandrapur": {
+        name: "Chandrapur",
+        assemblies: [
+            "70 - Rajura",
+            "71 - Chandrapur (SC)",
+            "72 - Ballarpur",
+            "73 - Bramhapuri",
+            "74 - Chimur",
+            "75 - Warora"
+        ]
+    },
+    "yavatmal": {
+        name: "Yavatmal",
+        assemblies: [
+            "76 - Wani",
+            "77 - Ralegaon (ST)",
+            "78 - Yavatmal",
+            "79 - Digras",
+            "80 - Arni (ST)",
+            "81 - Pusad",
+            "82 - Umarkhed (SC)"
+        ]
+    },
+    "nanded": {
+        name: "Nanded",
+        assemblies: [
+            "83 - Kinwat",
+            "84 - Hadgaon",
+            "85 - Bhokar",
+            "86 - Nanded North",
+            "87 - Nanded South",
+            "88 - Loha",
+            "89 - Naigaon",
+            "90 - Deglur (SC)",
+            "91 - Mukhed"
+        ]
+    },
+    "hingoli": {
+        name: "Hingoli",
+        assemblies: [
+            "92 - Basmath",
+            "93 - Kalamnuri",
+            "94 - Hingoli"
+        ]
+    },
+    "parbhani": {
+        name: "Parbhani",
+        assemblies: [
+            "95 - Jintur",
+            "96 - Parbhani",
+            "97 - Gangakhed",
+            "98 - Pathri"
+        ]
+    },
+    "jalna": {
+        name: "Jalna",
+        assemblies: [
+            "99 - Partur",
+            "100 - Gansavangi",
+            "101 - Jalna",
+            "102 - Badnapur (SC)",
+            "103 - Bhokardan"
+        ]
+    },
+    "aurangabad": {
+        name: "Aurangabad",
+        assemblies: [
+            "104 - Sillod",
+            "105 - Kannad",
+            "106 - Pholambari",
+            "107 - Aurangabad (Central)",
+            "108 - Aurangabad (West) (SC)",
+            "109 - Aurangabad (East)",
+            "110 - Paithan",
+            "111 - Gangapur",
+            "112 - Vaijapur"
+        ]
+    },
+    "nashik": {
+        name: "Nashik",
+        assemblies: [
+            "113 - Nandgaon",
+            "114 - Malegaon (Central)",
+            "115 - Malegaon (Outer)",
+            "116 - Baglan (ST)",
+            "117 - Kalwan (ST)",
+            "118 - Chandwad",
+            "119 - Yevla",
+            "120 - Sinnar",
+            "121 - Niphad",
+            "122 - Dindori (ST)",
+            "123 - Nashik East",
+            "124 - Nashik (Central)",
+            "125 - Nashik West",
+            "126 - Deolali (SC)",
+            "127 - Igatpuri (ST)"
+        ]
+    },
+    "palghar": {
+        name: "Palghar",
+        assemblies: [
+            "128 - Dahanu (ST)",
+            "129 - Vikramgad (ST)",
+            "130 - Palghar (ST)",
+            "131 - Boisar (ST)",
+            "132 - Nalasopara",
+            "133 - Vasai"
+        ]
+    },
+    "thane": {
+        name: "Thane",
+        assemblies: [
+            "134 - Bhiwandi Rural (ST)",
+            "135 - Shahapur (ST)",
+            "136 - Bhiwandi West",
+            "137 - Bhiwandi East",
+            "138 - Kalyan West",
+            "139 - Murbad",
+            "140 - Ambarnath (SC)",
+            "141 - Ulhasnagar",
+            "142 - Kalyan East",
+            "143 - Dombivali",
+            "144 - Kalyan Rural",
+            "145 - Meera Bhayandar",
+            "146 - Ovala Majiwada",
+            "147 - Kopri-Pachpakhadi",
+            "148 - Thane",
+            "149 - Mumbra-Kalwa",
+            "150 - Airoli",
+            "151 - Belapur"
+        ]
+    },
+    "mumbai-suburban": {
+        name: "Mumbai Suburban",
+        assemblies: [
+            "152 - Borivali",
+            "153 - Dahisar",
+            "154 - Magathane",
+            "155 - Mulund",
+            "156 - Vikhroli",
+            "157 - Bhandup West",
+            "158 - Jogeshwari East",
+            "159 - Dindoshi",
+            "160 - Kandivali East",
+            "161 - Charkop",
+            "162 - Malad West",
+            "163 - Goregaon",
+            "164 - Versova",
+            "165 - Andheri West",
+            "166 - Andheri East",
+            "167 - Vile Parle",
+            "168 - Chandivali",
+            "169 - Ghatkopar West",
+            "170 - Ghatkopar East",
+            "171 - Mankhurd Shivajinagar",
+            "172 - Anushakti Nagar",
+            "173 - Chembur",
+            "174 - Kurla (SC)",
+            "175 - Kalina",
+            "176 - Vandre East",
+            "177 - Vandre West"
+        ]
+    },
+    "mumbai-city": {
+        name: "Mumbai City",
+        assemblies: [
+            "178 - Dharavi (SC)",
+            "179 - Sion Koliwada",
+            "180 - Wadala",
+            "181 - Mahim",
+            "182 - Worli",
+            "183 - Shivadi",
+            "184 - Byculla",
+            "185 - Malabar Hill",
+            "186 - Mumbadevi",
+            "187 - Colaba"
+        ]
+    },
+    "raigad": {
+        name: "Raigad",
+        assemblies: [
+            "188 - Panvel",
+            "189 - Karjat",
+            "190 - Uran",
+            "191 - Pen",
+            "192 - Alibag",
+            "193 - Shrivardhan",
+            "194 - Mahad"
+        ]
+    },
+    "pune": {
+        name: "Pune",
+        assemblies: [
+            "195 - Junnar",
+            "196 - Ambegaon",
+            "197 - Khed Alandi",
+            "198 - Shirur",
+            "199 - Daund",
+            "200 - Indapur",
+            "201 - Baramati",
+            "202 - Purandar",
+            "203 - Bhor",
+            "204 - Maval",
+            "205 - Chinchwad",
+            "206 - Pimpri (SC)",
+            "207 - Bhosari",
+            "208 - Vadgaon Sheri",
+            "209 - Shivajinagar",
+            "210 - Kothrud",
+            "211 - Khadakwasala",
+            "212 - Parvati",
+            "213 - Hadapsar",
+            "214 - Pune Cantonment (SC)",
+            "215 - Kasba Peth"
+        ]
+    },
+    "ahmednagar": {
+        name: "Ahmednagar",
+        assemblies: [
+            "216 - Akole (ST)",
+            "217 - Sangamner",
+            "218 - Shirdi",
+            "219 - Kopargaon",
+            "220 - Shrirampur (SC)",
+            "221 - Nevasa",
+            "222 - Shevgaon",
+            "223 - Rahuri",
+            "224 - Parner",
+            "225 - Ahmednagar City",
+            "226 - Shrigonda",
+            "227 - Karjat Jamkhed"
+        ]
+    },
+    "beed": {
+        name: "Beed",
+        assemblies: [
+            "228 - Georai",
+            "229 - Majalgaon",
+            "230 - Beed",
+            "231 - Ashti",
+            "232 - Kaij (SC)",
+            "233 - Parli"
+        ]
+    },
+    "latur": {
+        name: "Latur",
+        assemblies: [
+            "234 - Latur Rural",
+            "235 - Latur City",
+            "236 - Ahmedpur",
+            "237 - Udgir (SC)",
+            "238 - Nilanga",
+            "239 - Ausa"
+        ]
+    },
+    "osmanabad": {
+        name: "Osmanabad",
+        assemblies: [
+            "240 - Omerga (SC)",
+            "241 - Tuljapur",
+            "242 - Osmanabad",
+            "243 - Paranda"
+        ]
+    },
+    "solapur": {
+        name: "Solapur",
+        assemblies: [
+            "244 - Karmala",
+            "245 - Madha",
+            "246 - Barshi",
+            "247 - Mohol (SC)",
+            "248 - Solapur City North",
+            "249 - Solapur City Central",
+            "250 - Akkalkot",
+            "251 - Solapur South",
+            "252 - Pandharpur",
+            "253 - Sangola",
+            "254 - Malshiran (SC)"
+        ]
+    },
+    "satara": {
+        name: "Satara",
+        assemblies: [
+            "255 - Phaltan (SC)",
+            "256 - Wai",
+            "257 - Koregaon",
+            "258 - Man",
+            "259 - Karad North",
+            "260 - Karad South",
+            "261 - Patan",
+            "262 - Satara"
+        ]
+    },
+    "ratnagiri": {
+        name: "Ratnagiri",
+        assemblies: [
+            "263 - Dapoli",
+            "264 - Guhagar",
+            "265 - Chiplun",
+            "266 - Ratnagiri",
+            "267 - Rajapur"
+        ]
+    },
+    "sindhudurg": {
+        name: "Sindhudurg",
+        assemblies: [
+            "268 - Kankavli",
+            "269 - Kudal",
+            "270 - Sawantwadi"
+        ]
+    },
+    "kolhapur": {
+        name: "Kolhapur",
+        assemblies: [
+            "271 - Chandgad",
+            "272 - Radhanagari",
+            "273 - Kagal",
+            "274 - Kolhapur South",
+            "275 - Karvir",
+            "276 - Kolhapur North",
+            "277 - Shahuwadi",
+            "278 - Hatkanangle (SC)",
+            "279 - Ichalkaranji",
+            "280 - Shirol"
+        ]
+    },
+    "sangli": {
+        name: "Sangli",
+        assemblies: [
+            "281 - Miraj (SC)",
+            "282 - Sangli",
+            "283 - Islampur",
+            "284 - Shirala",
+            "285 - Palus-Kadegaon",
+            "286 - Khanpur",
+            "287 - Tasgaon-Kavathe Mahankal",
+            "288 - Jat"
+        ]
+    }
+};
